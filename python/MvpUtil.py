7cdfb99161ee406fc610c768114f90ce52d4becc
DANIEL BENEVIDES <daniel@dbenev.com.br>
2024-09-26 07:18:05 -0300

    Create MvpUtil.py

---
+++ python/MvpUtil.py
@@ -0,0 +1,14 @@
+class MvpUtil:
+
+#    def __init__(self, nome, idade):
+#
+#        self.nome = nome
+#
+#        self.idade = idade
+
+    # Exibir Qtde de Linhas do Dataset
+    def printQtdLinhasDataFrame(df):
+        
+        lQtd = df.shape[0]
+        md(f'####{lQtd=}')
+        return lQtd
