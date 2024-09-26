094009f3e47b282c147f9171f323e7e46a320cc8
DANIEL BENEVIDES <daniel@dbenev.com.br>
2024-09-26 07:53:38 -0300

    Update MvpUtil.py

--- python/MvpUtil.py
+++ python/MvpUtil.py
@@ -6,8 +6,8 @@ class MvpUtil:
     #        self.idade = idade
 
     # Exibir Qtde de Linhas do Dataset
-    @staticmethod
     def printQtdLinhasDataFrame(df):
         lQtd = df.shape[0]
         md(f'####{lQtd=}')
         return lQtd
+    printQtdLinhasDataFrame = staticmethod(printQtdLinhasDataFrame)
